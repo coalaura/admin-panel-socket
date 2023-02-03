50761c709592572d0373ec6f7ac02c0f6848df50
Laura <milanbartky@gmail.com>
2023-02-03 03:02:15 +0100

    Cleanup historic data

---
+++ cleanup.js
@@ -0,0 +1,48 @@
+import moment from "moment";
+
+import { readdirSync, lstatSync } from "fs";
+import { join } from "path";
+import { execSync } from "child_process";
+
+function _cleanupServer(pServer) {
+	const now = moment();
+
+	const path = join("historic", pServer),
+		files = readdirSync(path);
+
+	let removed = 0;
+
+	for (const file of files) {
+		const filePath = join(path, file),
+			isDirectory = lstatSync(filePath).isDirectory();
+
+		if (isDirectory) {
+			const timestamp = moment(file, "DD-MM-YYYY"),
+				days = now.diff(timestamp, "days");
+
+			if (days > 10) {
+				execSync(`rm -rf ${filePath}`);
+
+				console.log(`  Cleaned up ${filePath} (${days} days)`);
+
+				removed++;
+			}
+		}
+	}
+
+	return removed;
+}
+
+export function cleanupHistoricData() {
+	console.log("Cleaning up historic data...");
+
+	const servers = readdirSync("historic");
+
+	let removed = 0;
+
+	for (const server of servers) {
+		removed += _cleanupServer(server);
+	}
+
+	console.log(`Cleanup complete (${removed} files removed)`);
+}
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { init, isValidLicense, isValidToken, isValidType } from "./data-loop.js"
 import { handleConnection, handleDataUpdate } from "./client.js";
 import { initRoutes } from "./routes.js";
 import { initServers, getServer } from "./server.js";
+import { cleanupHistoricData } from "./cleanup.js";
 
 import express from "express";
 import { createServer } from "http";
@@ -13,60 +14,64 @@ await initServers();
 
 init(handleDataUpdate);
 
+cleanupHistoricData();
+
+setInterval(cleanupHistoricData, 2 * 60 * 60 * 1000);
+
 const app = express(),
-    server = createServer(app);
+	server = createServer(app);
 
 app.use(cors({
-    origin: '*'
+	origin: '*'
 }));
 
 initRoutes(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
+	}
 });
 
 io.on("connection", client => {
-    const query = client.handshake.query;
+	const query = client.handshake.query;
 
-    if (!('server' in query) || !('token' in query) || !('type' in query) || !('license' in query) || !isValidType(query.type) || !isValidLicense(query.license)) {
-        client.emit("message", "Invalid request");
+	if (!('server' in query) || !('token' in query) || !('type' in query) || !('license' in query) || !isValidType(query.type) || !isValidLicense(query.license)) {
+		client.emit("message", "Invalid request");
 
-        client.disconnect(true);
+		client.disconnect(true);
 
-        console.log(`${chalk.redBright("Rejected connection")} ${chalk.gray("from " + client.handshake.address)}`);
+		console.log(`${chalk.redBright("Rejected connection")} ${chalk.gray("from " + client.handshake.address)}`);
 
-        return;
-    }
+		return;
+	}
 
-    const server = getServer(query.server);
+	const server = getServer(query.server);
 
-    if (!server) {
-        client.emit("message", "Invalid server");
+	if (!server) {
+		client.emit("message", "Invalid server");
 
-        client.disconnect(true);
+		client.disconnect(true);
 
-        console.log(`${chalk.redBright("Invalid server")} ${chalk.gray("from " + client.handshake.address)}`);
-    }
+		console.log(`${chalk.redBright("Invalid server")} ${chalk.gray("from " + client.handshake.address)}`);
+	}
 
-    isValidToken(query.server, query.token).then(valid => {
-        if (!valid) {
-            client.emit("message", "Invalid session");
+	isValidToken(query.server, query.token).then(valid => {
+		if (!valid) {
+			client.emit("message", "Invalid session");
 
-            client.disconnect(true);
+			client.disconnect(true);
 
-            console.log(`${chalk.redBright("Invalid token")} ${chalk.gray("from " + client.handshake.address)}`);
+			console.log(`${chalk.redBright("Invalid token")} ${chalk.gray("from " + client.handshake.address)}`);
 
-            return;
-        }
+			return;
+		}
 
-        handleConnection(client, server.server, query.type, query.license);
-    });
+		handleConnection(client, server.server, query.type, query.license);
+	});
 });
 
 server.listen(9999, () => {
-    console.log(chalk.blueBright("Listening for sockets..."));
+	console.log(chalk.blueBright("Listening for sockets..."));
 });
