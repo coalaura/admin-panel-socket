66343f265b20ccdaeff7cda1c4fe814ca20f15e4
Laura <milanbartky@gmail.com>
2023-08-27 13:02:42 +0200

    fix models

--- models.js
+++ models.js
@@ -1,5 +1,13 @@
 import {requestOpFwApi} from "./http.js";
 
 export async function updateModelsJSON(pServer) {
-    pServer.models = await requestOpFwApi(`${pServer.url}/op-framework/models.json`, pServer.token);
+    const models = await requestOpFwApi(`${pServer.url}/op-framework/models.json`, pServer.token);
+
+    pServer.models = models.models;
+
+    for (const weaponHash in pServer.weapons) {
+        const weaponName = pServer.weapons[weaponHash];
+
+        pServer.models[weaponHash] = weaponName;
+    }
 }
