105663bf62f39a042c7008157c062423f8428d8e
Laura <milanbartky@gmail.com>
2025-07-04 16:29:08 +0200

    fix

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 
 const configData = JSON.parse(readFileSync("./_config.json", "utf8"));
 
-if (process.argv.includes("--docker")) {
+if (process.env.DOCKER_MODE) {
 	const cluster = process.env.CLUSTER;
 
 	if (!cluster) throw new Error("missing cluster");
