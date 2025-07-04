b6da2365f7ecda5435b974de37b8f78c739b50e2
Laura <milanbartky@gmail.com>
2025-07-04 16:13:46 +0200

    tweak

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 
 const configData = JSON.parse(readFileSync("./_config.json", "utf8"));
 
-if (process.env.DOCKER_MODE === "true") {
+if (process.env.DOCKER_MODE) {
 	const cluster = process.env.CLUSTER;
 
 	if (!cluster) throw new Error("missing cluster");
