3158c5337cc74af4e66a3f09e76caef5ca61e9c2
Laura <milanbartky@gmail.com>
2025-07-05 02:00:48 +0200

    read influx token from env

--- config.js
+++ config.js
@@ -3,13 +3,19 @@ import { readFileSync } from "node:fs";
 const configData = JSON.parse(readFileSync("./_config.json", "utf8"));
 
 if (process.env.DOCKER_MODE) {
-	const cluster = process.env.DOCKER_CLUSTER;
+	const cluster = process.env.DOCKER_CLUSTER,
+		influxToken = process.env.DOCKER_INFLUX_TOKEN;
 
 	if (!cluster) throw new Error("missing cluster");
 
 	configData.docker = true;
 	configData.panel = "/etc/opfw-config";
 	configData.servers = [cluster];
+
+	configData.influx = influxToken ? {
+		url: "http://influxdb:8086",
+		token: influxToken
+	} : null;
 
 	console.log(`Docker mode enabled with cluster ${cluster}!`);
 }
